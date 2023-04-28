0d377d2394c26ff6afab515ff298b56527b35384
xieyuschen <xieyuschen@gmail.com>
2023-04-28 15:45:00 +0800

    try to create pipeline and check exception

--- shgpt/Main.hs
+++ shgpt/Main.hs
@@ -1,25 +1,52 @@
 {-# LANGUAGE BlockArguments #-}
 import System.Process
 import Data.Text.IO (hGetLine)
-import GHC.IO.Handle (isEOF, hGetLine)
+import GHC.IO.Handle (isEOF, hGetLine, hIsEOF, hIsClosed)
 import GHC.IO.Handle.Types (Handle)
 import Data.Type.Equality (outer)
+import Control.Exception
+import System.IO
 
 main :: IO ()
 main = do
-  (_, out , _, _) <- createProcess (proc "ls" []){ std_out = CreatePipe }
-  myLoop out
+  -- create a named pipe if not exsit
+  result <- try' $ createProcess (proc "mkfifo" ["try"]){ std_out = CreatePipe }
+  case result of
+    Left e -> do
+      print e
+    Right (_, out , _, p) -> do
+      myLoop out
+  print "end"
+
+try' :: IO a -> IO (Either IOException a)
+try' = try
+
+nothingIfEmtpy :: String -> Maybe String
+nothingIfEmtpy "" = Nothing
+nothingIfEmtpy x = Just x
+
 
 myLoop :: Maybe Handle -> IO ()
-myLoop input = do 
-  done <- isEOF
-  if done
-    then return ()
-    else do 
-      let tmp = GHC.IO.Handle.hGetLine <$> input
-      case tmp of
-        Nothing -> return ()
-        Just x -> do
-            x >>= putStrLn
-            myLoop input
--- a -> m b -> m a 
+myLoop input = do
+    -- get line will fail because there is no line to read
+    tmp <- case input of
+        Nothing -> return $ Left $ userError "encounter the end of line"
+        Just h -> do
+          eof <- hIsEOF h
+          if eof
+            then return $ Right Nothing
+            else do
+              result <- try' $ GHC.IO.Handle.hGetLine h
+              return $ case result of
+                Left e -> Left e
+                Right x -> Right $ Just x
+
+    case tmp of
+      Left e -> do
+        putStrLn $ "encounter user error:" ++ show e
+        pure ()
+      Right Nothing -> pure ()
+      Right (Just x) -> do
+        putStrLn $ "command output:" ++ x
+        myLoop input
+        
